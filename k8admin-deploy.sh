aca388fdd9310a5a76aa2cb24b2c1aace96d525b
gabrielefronze <sucre.91@hotmail.it>
2019-10-10 19:53:43 +0200

    Setting Docker to user systemd groups

--- k8admin-deploy.sh
+++ k8admin-deploy.sh
@@ -1,13 +1,47 @@
 #! /bin/bash
 
-touch k8admin-deploy.log
+echo ""
+echo "###########################################################################"
+echo "Setting up Docker to use systemd groups"
+echo "###########################################################################"
+echo ""
+cat > /etc/docker/daemon.json <<EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2",
+  "storage-opts": [
+    "overlay2.override_kernel_check=true"
+  ]
+}
+EOF
 
+mkdir -p /etc/systemd/system/docker.service.d
+
+systemctl daemon-reload
+systemctl restart docker
+
+echo ""
+echo "###########################################################################"
 echo "Pulling config images"
+echo "###########################################################################"
+echo ""
 kubeadm config images pull >> k8admin-deploy.log
 
+echo ""
+echo "###########################################################################"
 echo "Initializing k8s single control plane cluster backed by Flannel"
+echo "###########################################################################"
+echo ""
 HOST_IP=`hostname --ip-address`
 kubeadm init --control-plane-endpoint=$HOST_IP --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address=$HOST_IP >> k8admin-deploy.log
 
+echo ""
+echo "###########################################################################"
 echo "Deploying Flannel network layer"
+echo "###########################################################################"
+echo ""
 kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml >> k8admin-deploy.log
